005023aed9828cfd42dfbd4daa8f9a00acd59ce5
Santiago <s.celisr2@uniandes.edu.co>
2023-10-01 21:02:40 -0500

    Arreglo

--- src/main/java/uniandes/edu/co/proyecto/Controller/InternetsController.java
+++ src/main/java/uniandes/edu/co/proyecto/Controller/InternetsController.java
@@ -1,6 +1,5 @@
 package uniandes.edu.co.proyecto.Controller;
 
-import org.hibernate.internal.util.type.PrimitiveWrapperHelper.IntegerDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
