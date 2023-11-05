8bc0840fead09a924c494b385cb6441b07385879
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2023-11-05 10:29:32 -0500

    Data_salones

---
+++ data/Data_Salones.sql
@@ -0,0 +1,10 @@
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (1, 40, '565.80', true, '48.66', 67);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (2, 75, '762.46', true, '116.03', 155);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (3, 110, '502.61', true, '122.66', 144);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (4, 153, '579.54', false, '0', 177);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (5, 180, '931.26', false, '0', 175);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (6, 1, '370.95', false, '0', 120);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (7, 133, '313.00', true, '97.78', 165);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (8, 113, '512.06', true, '77.42', 166);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (9, 85, '424.90', true, '83.04', 159);
+insert into MOCK_DATA (ID, Capacidad, Costo, tieneCostoAdicional, CostoAdicional, tiempoDeLimpieza) values (10, 145, '205.30', false, '0', 178);
